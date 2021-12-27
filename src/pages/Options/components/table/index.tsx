182f9ba52b9b064f99bfcea0fdd132f127866e91
Yash Kumar Verma <yk.verma2000@gmail.com>
2021-12-27 09:49:08 +0530

    chore: rely on imported interfaces

--- src/pages/Options/components/table/index.tsx
+++ src/pages/Options/components/table/index.tsx
@@ -1,41 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { content } from "../../content"
+import { StoredWord } from "../../interfaces/storedWord"
 
 interface Props { }
 
-/**
- * trace stores the instances when the event was fired
- * 
- * the reason that traces are kept separate from StoredWords are
- * - traces will grow very quickly as the user adds words and browses the web
- * - once traces become bulky, it would slow down normal text lookup and search
- * - there is no need of traces in normal extension working, so no need to keep in primary storage media
- * - traces are sent to telemetry endpoint and can be used to analyze the usage of extension
- * - traces can be cleared as needed by user.
- */
-interface Trace {
-    id: number,
-    word: string,
-    url: string;
-    extra: any
-}
-
-/**
- * StoredWord is the primary data index of the extension
- * 
- * - single source of truth for all the words and extension logic
- * - id acts as primary key, implemented on application layer
- * - title is the actual word string that is being stored
- * - status allows quick toggle of the word's status
- * - count : an "approximate" of number of times a word was encountered by user.
- * - count increments w.r.t traces but persist when traces are cleared.
- */
-interface StoredWord {
-    id: number;
-    title: string;
-    status: boolean;
-    count: number,
-}
 
 /**
  * Table to display a dynamic list of words which are blocked by the extension.
